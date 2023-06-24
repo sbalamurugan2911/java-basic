75bda11d7519ccc7de56031e4fe7a7e8ac93a1bb
balamurugan <sbalamurugan2911@gmail.com>
2023-06-24 12:54:20 +0530

    basic

---
+++ src/javaProgram/Swapping.java
@@ -0,0 +1,21 @@
+package javaProgram;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int a=-2,b=0,c=10,d;
+		
+		System.out.println("Before Swapping");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		d=a;
+		a=b;
+		b=c;
+		c=d;
+		System.out.println("After Swapping");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+}
+}
