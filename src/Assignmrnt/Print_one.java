98a2a2433dda4f60af4a1a53517077a46112dd06
balamurugan <sbalamurugan2911@gmail.com>
2023-06-29 21:54:33 +0530

    loop basic

---
+++ src/Assignmrnt/Print_one.java
@@ -0,0 +1,14 @@
+package Assignmrnt;
+
+public class Print_one {
+
+	public static void main(String[] args) {
+		// printing 1 for 5 times
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(1);
+
+		}
+
+	}
+
+}
---
+++ src/Assignmrnt/Printing_numbers.java
@@ -0,0 +1,13 @@
+package Assignmrnt;
+
+public class Printing_numbers {
+
+	public static void main(String[] args) {
+		// printing numbers using loop
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
