1c74750119f4ab7bde1811b61cbabd0787c4d309
balamurugan <sbalamurugan2911@gmail.com>
2023-06-29 11:15:43 +0530

    while loop

---
+++ src/looping/WhileLoop.java
@@ -0,0 +1,15 @@
+package looping;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int a=2,range=22;
+	
+		while(a<=range) {
+			System.out.println(a);
+			a+=2;
+		}
+
+	}
+
+}
