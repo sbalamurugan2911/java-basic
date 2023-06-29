d52e95d1f76f1855b0c403896f5114578d3c5e67
balamurugan <sbalamurugan2911@gmail.com>
2023-06-29 21:57:16 +0530

    basic loop

---
+++ src/Assignmrnt/Printing_even_number.java
@@ -0,0 +1,14 @@
+package Assignmrnt;
+
+public class Printing_even_number {
+
+	public static void main(String[] args) {
+		// printing even number
+				for(int i=1;i<=10;i++) {
+					if(i%2==0)
+						System.out.println(i);
+				}
+
+	}
+
+}
---
+++ src/Assignmrnt/Printing_odd_number.java
@@ -0,0 +1,14 @@
+package Assignmrnt;
+
+public class Printing_odd_number {
+
+	public static void main(String[] args) {
+		// Printing odd number
+		for(int i=1;i<=10;i++) {
+			if(i%2==1)
+				System.out.println(i);
+		}
+
+	}
+
+}
