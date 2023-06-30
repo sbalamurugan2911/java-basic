409dbbe75352078ea33fb19127231742634494dd
balamurugan <sbalamurugan2911@gmail.com>
2023-06-30 12:09:18 +0530

    basic loop

---
+++ src/Assignmrnt/Printing1_8_27_64.java
@@ -0,0 +1,12 @@
+package Assignmrnt;
+
+public class Printing1_8_27_64 {
+
+	public static void main(String[] args) {
+		// printing 1 8 27 64
+		for(int i = 1;i<=10;i++)
+			System.out.print((i*i*i)+" ");
+
+	}
+
+}
