5bf2f3d0fb0cba0603506967219db6e0bd86438e
balamurugan <sbalamurugan2911@gmail.com>
2023-07-03 11:49:29 +0530

    basic loop

---
+++ src/Assignmrnt/Problem20.java
@@ -0,0 +1,17 @@
+package Assignmrnt;
+
+public class Problem20 {
+
+	public static void main(String[] args) {
+		// square root
+		double num =1444,i=2;
+		while(i<=num/2) {
+			if(num/i==i) {
+				System.out.println(i);
+			}
+			i++;
+		}
+
+	}
+
+}
