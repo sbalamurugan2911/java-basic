901387185bd057317f6c10311170c802b7aa9a97
balamurugan <sbalamurugan2911@gmail.com>
2023-07-06 16:17:37 +0530

    loop problem

--- src/Assignment1/Problem35.java
+++ src/Assignment1/Problem35.java
@@ -4,8 +4,21 @@ public class Problem35 {
 
 	public static void main(String[] args) {
 		// strong number (number=sum of digits factorial)
-		
+		int num = 145, sum = 0, factorial = 1, num1 = num, remainder;
+		while (num > 0) {
+			remainder = num % 10;
+			for (int i = remainder; i > 0; i--) {
+				factorial *= i;
+			}
+			sum += factorial;
+			factorial = 1;
+			num /= 10;
+		}
 
+		if (num1 == sum) {
+			System.out.println(num1 + " is strong number");
+		} else
+			System.out.println(num1 + " is not strong number");
 	}
 
 }
