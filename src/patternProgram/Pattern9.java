764ae8d179a64920ec5d6eac8aaa3510e9fbcd5d
balamurugan <sbalamurugan2911@gmail.com>
2023-07-08 20:19:03 +0530

    pattern program

---
+++ src/patternProgram/Pattern9.java
@@ -0,0 +1,24 @@
+package patternProgram;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1 
+		 * 2 3 
+		 * 4 5 6 
+		 * 7 8 9 10
+		 */
+		int num = 1;
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(num + " ");
+				num++;
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
