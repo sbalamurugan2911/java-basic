3654d08e9d3fa019ed163f89103ac352d55920c9
balamurugan <sbalamurugan2911@gmail.com>
2023-07-09 19:11:37 +0530

    pattern program

---
+++ src/patternProgram/Pattern10.java
@@ -0,0 +1,23 @@
+package patternProgram;
+
+public class Pattern10 {
+
+	public static void main(String[] args) {
+		/*
+		       1
+		       1 2
+		       1 2 3
+		       1 2 3 4
+		 
+		*/
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			
+			}
+			System.out.println();
+		}
+
+	}
+
+}
