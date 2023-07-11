bcf145bebc19a1e6ef90f174ad4010aa26c7dcac
balamurugan <sbalamurugan2911@gmail.com>
2023-07-11 16:33:59 +0530

    pattern program

---
+++ src/patternProgram/Pattern8.java
@@ -0,0 +1,23 @@
+package patternProgram;
+
+public class Pattern8 {
+
+	public static void main(String[] args) {
+		/*
+		           * 
+		        *  *  * 
+		           * 
+		  
+		*/
+		for(int i=1;i<=3;i++) {
+			for(int j=1;j<=3;j++) {
+				if(i>=1 && j>=1 && i!=2 && j!=2) {
+					System.out.print(" "+" ");
+				}else
+				System.out.print("*"+ " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
