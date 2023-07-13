708aaa0c075c546c6d4834c7bf3df8b8c4c0df1e
balamurugan <sbalamurugan2911@gmail.com>
2023-07-13 15:25:08 +0530

    array program

---
+++ src/array/Program1.java
@@ -0,0 +1,26 @@
+package array;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// Array declaration
+		int array[];
+		
+		array= new int[5];              
+		
+		array [0]=1;
+		array [1]=2;                 
+		array [2]=3;
+		array [3]=4;
+		array [4]=5;
+		
+		// easy method to declare array
+		
+		int array1[]= {1,2,3,4,5};
+			
+		
+		 
+
+	}
+
+}
